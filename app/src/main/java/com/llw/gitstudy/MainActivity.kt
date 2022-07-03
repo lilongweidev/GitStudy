434b418c6f1b224f99843f099f67886e69f20b7a
llw <lonelyholiday@qq.com>
2022-07-03 12:06:41 +0800

    添加静态权限注册。

--- app/src/main/java/com/llw/gitstudy/MainActivity.kt
+++ app/src/main/java/com/llw/gitstudy/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.llw.gitstudy
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestCamera =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(this, if (it) "权限授予" else "未授予", Toast.LENGTH_SHORT).show()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestCamera.launch(Manifest.permission.CAMERA)
     }
 }
