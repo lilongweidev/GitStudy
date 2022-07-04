f620c0ec087135ad7144ca84f84d147321143920
llw <lonelyholiday@qq.com>
2022-07-05 01:22:48 +0800

    Test

---
+++ app/src/main/java/com/llw/gitstudy/ToastUtils.kt
@@ -0,0 +1,10 @@
+package com.llw.gitstudy
+
+import android.content.Context
+import android.widget.Toast
+
+fun String.showToast(context: Context) =
+    Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
+
+fun String.showLongToast(context: Context) =
+    Toast.makeText(context, this, Toast.LENGTH_LONG).show()
